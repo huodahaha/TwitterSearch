b4ab0249c4071aa05bcedc839dc3dc73268d9837
HuoDa <dh2582@nyu.edu>
2017-04-30 04:54:35 -0400

    update

--- unnitest.py
+++ unnitest.py
@@ -1,10 +1,4 @@
 from TwitterSearch.test.unittest import *
 
 if __name__ == "__main__":
-    reset_database()
-
-    # print "related keyword:"
-    # test_related_users_keywords()
-
-    print "self keyword:"
     test_search_text()
