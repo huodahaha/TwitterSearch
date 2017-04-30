a6ab0255bc550bd086fc0480b9890557746d642f
HuoDa <dh2582@nyu.edu>
2017-04-30 03:47:01 -0400

    增加安装脚本

---
+++ install.sh
@@ -0,0 +1,2 @@
+pip install -r ./requirments.txt
+python -m nltk.downloader -d /usr/share/nltk_data wordnet
---
+++ unnitest.py
@@ -0,0 +1,10 @@
+from TwitterSearch.test.unittest import *
+
+if __name__ == "__main__":
+    reset_database()
+
+    # print "related keyword:"
+    # test_related_users_keywords()
+
+    print "self keyword:"
+    test_search_text()
